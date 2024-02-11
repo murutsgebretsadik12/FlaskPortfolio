ede2ac531a1260b2c671f0994ad3c02ae2ff9e81
murutsmenge <murutsmenge21@gmail.com>
2024-02-11 16:24:59 +0000

    update the script

--- static/js/script.js
+++ static/js/script.js
@@ -6,3 +6,15 @@ menuBtn.addEventListener('click', function(e){
     document.querySelector('body').classList.toggle('mobile_nav_active');
     this.classList.toggle('fa-xmark')
 } )
+
+//typejs initailize
+
+let typed = new Typed('.auto_input', {
+	strings: ['Muruts Gebretsadik.', 'Web Developer.','Freelancer.', 'UI Designer.'],
+	typeSpeed: 60,
+	backSpeed:60,
+	loop: true,
+	loopCount: Infinity,
+	startDelay:500,
+	
+});
