998b53a8f2c7821abec5aaf182ddb5efeab3dbf1
murutsmenge <murutsmenge21@gmail.com>
2024-02-13 10:16:29 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template,url_for
 app =Flask(__name__)
 
+
+
+
+
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 
 @app.route('/skill')
 def skill():
